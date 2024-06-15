a408f316b1a7dc79efef8d163be1992064708d71
nachoeg <nachoeg99@gmail.com>
2024-06-15 08:41:28 -0300

    update padding and gaps in header and projectsGrid

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -16,7 +16,7 @@ export function ProjectsGrid() {
   const filteredProjects = filterProjects();
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
       {filteredProjects.map((project, index) => (
         <Project key={index} data={project} />
       ))}
