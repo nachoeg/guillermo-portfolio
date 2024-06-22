2204e5f2f0588649fc6ba35a12e62efb935e8079
nachoeg <nachoeg99@gmail.com>
2024-06-22 05:11:05 -0300

    update projectGrid

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -1,15 +1,16 @@
-import PROJECTS from "@/data/projects.js";
 import { Project } from "@/components/Project";
 import { useStore } from "@nanostores/react";
 import { tagStore } from "../store.js";
 
-export function ProjectsGrid() {
+export function ProjectsGrid({ projects }) {
   const selectedTag = useStore(tagStore);
   function filterProjects() {
     if (selectedTag === "Todo") {
-      return PROJECTS;
+      return projects;
     } else {
-      return PROJECTS.filter((project) => project.tags.includes(selectedTag));
+      return projects.filter((project) => {
+        if (project.tags) return project.tags.includes(selectedTag);
+      });
     }
   }
 
@@ -17,9 +18,16 @@ export function ProjectsGrid() {
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
-      {filteredProjects.map((project, index) => (
-        <Project key={index} data={project} />
-      ))}
+      {!filteredProjects || filteredProjects.length === 0 ? (
+        <div className="col-span-full h-96 rounded-lg place-content-center text-center text-lg text-neutral-800 dark:text-neutral-300">
+          No se encontraron proyectos
+        </div>
+      ) : (
+        filteredProjects.map((project, index) => (
+          <Project key={project.id} data={project} index={index} />
+        ))
+      )}
+      {}
     </div>
   );
 }
