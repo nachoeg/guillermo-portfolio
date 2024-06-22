c0ba2d5473404432951284c0744b831e162307ad
nachoeg <nachoeg99@gmail.com>
2024-06-22 05:14:38 -0300

    update tags

--- src/components/TagSelector.jsx
+++ src/components/TagSelector.jsx
@@ -2,11 +2,13 @@ import TAGS from "@/data/tags.js";
 import { useStore } from "@nanostores/react";
 import { Badge } from "@/components/ui/badge.tsx";
 import { tagStore } from "../store.js";
+import { X } from "lucide-react";
 
 export function TagSelector() {
   const handleClick = (tag) => {
-    tagStore.set(tag.target.innerText);
-    console.log(tagStore.get());
+    if (tag.target.innerText == tagStore.get()) {
+      tagStore.set("Todo");
+    } else tagStore.set(tag.target.innerText);
   };
 
   const selectedTag = useStore(tagStore);
@@ -17,9 +19,14 @@ export function TagSelector() {
         <Badge
           key={tag}
           onClick={handleClick}
-          className="cursor-pointer select-none h-fit"
+          className={`group cursor-pointer select-none transition-all duration-300 h-fit gap-1 shadow ${
+            selectedTag == tag && selectedTag != "Todo" ? "ps-1" : ""
+          }`}
           variant={selectedTag == tag ? "default" : "secondary"}
         >
+          {selectedTag == tag && selectedTag != "Todo" && (
+            <X className="size-3 stroke-[2] rounded-full bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100 group-hover:text-neutral-700 dark:group-hover:text-neutral-300 transition pointer-events-none"></X>
+          )}
           {tag}
         </Badge>
       ))}
