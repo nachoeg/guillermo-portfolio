48f899f2b166047896161f2dd69f53874a5d4139
nachoeg <nachoeg99@gmail.com>
2024-06-24 23:57:17 -0300

    update add api

--- src/pages/api/project/add.ts
+++ src/pages/api/project/add.ts
@@ -6,47 +6,22 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
   console.log(formData);
   const title = formData.get("title")?.toString();
-  const description = formData.get("description")?.toString();
-  const tags = formData.get("tags")?.toString();
-  const images = [
-    "https://jcxgnvapdivmgjlgzgmc.supabase.co/storage/v1/object/sign/images/pipo.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvcGlwby5qcGciLCJpYXQiOjE3MTg2MDkxOTIsImV4cCI6MzMyNTQ2MDkxOTJ9.HFJcswWMfqBXpz9NPDELlvQszdo3SeSIjVnGSqsrbQ0&t=2024-06-17T07%3A26%3A32.832Z",
-  ];
-  // const images = formData.getAll("images");
-
-  // if (!title || !description || !tags || !images) {
-  //   return new Response("All fields are required", { status: 400 });
-  // }
-
-  // const imagesUrls: string[] = [];
-
-  // // añadir imagenes al storage primero
-  // images.forEach(async (image) => {
-  //   const { data, error } = await supabase.storage.from("images").upload(
-  //   if (error) {
-  //     return new Response(error.message, { status: 500 });
-  //   } else {
-  //     imagesUrls.push(data.fullPath);
-  //   }
-  // });
-
-  // // añadir proyecto a la base de datos
-
+  const tags = JSON.parse(formData.get("tags")?.toString() || "[]");
+  const images = JSON.parse(formData.get("images")?.toString() || "[]");
+  if (tags === "[]" || formData.get("images") === "[]")
+    return new Response("All fields are required", { status: 400 });
   const response = await supabase
     .from("projects")
     .insert([
       {
         title,
-        description,
         tags,
         images,
-        //
-        // images: imagesUrls,
       },
     ])
     .select();
   console.log(response);
-  // if (response.error)
-  //   return new Response(response.error.message, { status: 500 });
-  // return new Response(JSON.stringify(response.data), { status: 200 });
-  return redirect("/dashboard");
+  if (response.error)
+    return new Response(response.error.message, { status: 500 });
+  return new Response(JSON.stringify(response.data), { status: 200 });
 };
