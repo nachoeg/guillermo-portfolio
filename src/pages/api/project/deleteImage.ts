06912a6f2c0a77de378502f386877c227db0eb85
nachoeg <nachoeg99@gmail.com>
2024-07-11 17:05:05 -0300

    add API route for deleting images

---
+++ src/pages/api/project/deleteImage.ts
@@ -0,0 +1,26 @@
+import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase";
+
+export const DELETE: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const id = formData.get("id")?.toString();
+  if (id === undefined)
+    return new Response("El id de la imagen es indefinido", { status: 400 });
+  const { error: errorDeleteStorage } = await supabase.storage
+    .from("images")
+    .remove([id]);
+  if (errorDeleteStorage) {
+    console.log(errorDeleteStorage);
+    return new Response(errorDeleteStorage.message, { status: 500 });
+  }
+  const { error: errorDeleteImage } = await supabase
+    .from("images")
+    .delete()
+    .eq("id", id);
+  if (errorDeleteImage) {
+    console.log(errorDeleteImage);
+    return new Response(errorDeleteImage.message, { status: 500 });
+  }
+
+  return new Response("Imagen eliminada", { status: 200 });
+};
