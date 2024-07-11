549955cce4ded5d5565f1b18a71f073f163f3cdd
nachoeg <nachoeg99@gmail.com>
2024-07-11 19:09:49 -0300

    prueba de rendimiento

--- src/components/ProjectImage.tsx
+++ src/components/ProjectImage.tsx
@@ -10,19 +10,19 @@ function ProjectImage({ image, index }: { image: string; index: number }) {
   return (
     <>
       {!loaded && <Loading />}
-      <img
+      {/* <img
         src={image}
         className={`absolute aspect-auto object-cover pointer-events-none w-full h-full blur-3xl brightness-50 border-0 ${
           loaded ? "opacity-100" : "opacity-0"
         }`}
         alt={`Project ${index + 1} background`}
-      />
+      /> */}
       <img
         src={image}
         onLoad={handleImageLoad}
         className={`max-w-screen max-h-screen carouselImage md:max-h-[calc(100vh-20px)] object-contain z-10 border-0 md:transition ${
           loaded ? "opacity-100" : "opacity-0"
-        }`}
+        } bg-[url('https://placehold.co/600x400')]`}
         alt={`Project ${index + 1}`}
       />
     </>
