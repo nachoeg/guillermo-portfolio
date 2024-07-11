797f62d391eac40adff8ef3ace5106ab653ab844
nachoeg <nachoeg99@gmail.com>
2024-07-11 13:48:35 -0300

    add ProjectDashboard component

---
+++ src/components/ProjectDashboard.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Edit, Plus, Trash } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import ProjectEditButton from "./ProjectEditButton";
+
+function ProjectDashboard({
+  title,
+  current,
+  count,
+  tags,
+}: {
+  title: string;
+  current: number;
+  count: number;
+  tags: string[];
+}) {
+  const handleDelete = () => {
+    //borra la imagen actual y si es la unica borra el proyecto
+    console.log("Delete", current);
+  };
+
+  const handleAdd = () => {
+    //agrega una imagen al final
+    console.log("Add", current);
+  };
+  return (
+    <div className="absolute bottom-3 right-3 flex gap-2 [&_button]:shadow ">
+      <ProjectEditButton title={title} tags={tags}></ProjectEditButton>
+
+      <Button variant={"carousel"} size={"icon"} onClick={handleDelete}>
+        <Trash className="size-5" />
+      </Button>
+      <Button variant={"carousel"} size={"icon"} onClick={handleAdd}>
+        <Plus className="size-6" />
+      </Button>
+    </div>
+  );
+}
+
+export default ProjectDashboard;
